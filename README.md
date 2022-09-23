3265db9f09813d97cfea6825200b19ba53522602
Anton Alfsson <anton.alfsson@gmail.com>
2022-09-23 09:27:57 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+# dotfiles
+
+# Font
+
+terminal: Hack Nerd Font
+other: SF Pro Text
+
+# Icon set
+
+https://github.com/vinceliuice/Tela-icon-theme
+
+Install and use with `lxappearance`
+
+# Font
+
+Use SF Pro via `lxappearance`
+
+# Cursor
+
+https://github.com/TheScrawl/xcursor-breeze-neutral
+
+Run to `sudo cp -r xcursor-breeze-neutral /usr/share/icons/`
+
+Select via `lxappearance`
+
+# Firefox
+
+Configure fonts and don't allow pages to use their own.
---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# upgrade packages
+xbps-install -Syu
+
+# install stow
+xbps-install -Sy stow
+
+# install all configs
+cd packages
+stow -t $HOME *
+cd ..
+
+# install other required applications
+xbps-install -Sy $(cat pkgs | xargs)
+
+# install fisher
+fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
+
+# install z
+fish -c "fisher install jethrokuan/z"
+
+# install fzf
+fish -c "fisher install jethrokuan/fzf"
+
+#install sdkman fish support
+fish -c "fisher install reitzig/sdkman-for-fish@1.4.0"
+
+#install sdkman
+fish -c "curl -s \"https://get.sdkman.io\" | bash"
+
+fish -c "source \"$HOME/.sdkman/bin/sdkman-init.sh\""
+
+fish -c "sdk install java"
+
+fish -c "sdk install gradle 7.5.1"
+
+fish -c "sdk install maven"
+
+
+# install packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
