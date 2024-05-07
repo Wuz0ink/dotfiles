b0d0e5e16d9f7d5f4538a8c9b548621d02df65f0
Anton Alfsson <anton.alfsson@gmail.com>
2024-05-07 08:01:02 +0200

    Update top-panel.lua

--- packages/awesome/.config/awesome/components/top-panel.lua
+++ packages/awesome/.config/awesome/components/top-panel.lua
@@ -19,6 +19,7 @@ local dpi = beautiful.xresources.apply_dpi
 -- import widgets
 local task_list = require("widgets.task-list")
 local tag_list = require("widgets.tag-list")
+local battery_widget = require("awesome-wm-widgets.battery-widget.battery")
 
 -- define module table
 local top_panel = {}
@@ -48,6 +49,7 @@ top_panel.create = function(s)
         layout = wibox.layout.fixed.horizontal,
         tag_list.create(s),
         task_list.create(s),
+        battery_widget(),
       },
       require("widgets.calendar").create(s),
       {
